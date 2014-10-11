c921047274e0c5803c8818e3cbec0538f4cce91e
RokitSalad <peterdobbs@hotmail.com>
2014-10-11 15:37:01 +0100

    Pulled out the eventlogging assembly into a nuget dependency

--- Helpful.EventStore.EventLogging/StorableEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Helpful.EventStore.EventLogging
-{
-    public class StorableEvent
-    {
-        public string Stream { get; set; }
-        public string Category { get; set; }
-        public object Data { get; set; }
-    }
-}
